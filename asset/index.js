675a7c751b105e366ee7a9a2e3c45ceeee051ab8
wurui <wurui.wr@gmail.com>
2017-06-23 10:10:48 +0800

    first commit

--- asset/index.js
+++ asset/index.js
@@ -1,6 +1,79 @@
-define([],function(){
+define(['zepto','mustache'],function(undef,Mustache){
+
+    var timeKey=function(date){
+        date =date ||new Date();
+        return (date.getFullYear()*10000 +(date.getMonth()+1)*100 + date.getDate()).toString();
+    };
+    var $list,tpl;
+        getData=function(pid,fn){
+            $.getJSON('/admin/page/getdetailpv?pid='+pid+'&TimezoneOffset=-480',function(r){
+                fn(r && r.data,pid)
+            })
+        },
+        pageConf=[
+            {
+                pid:'58f97db80304656d7a905d0d',
+                title:'首页'
+            }, {
+                pid:'5922b424940282a75fa82ac2',
+                title:'制作页'
+            }, {
+                pid:'58fb40ca0304656d7a905e16',
+                title:'购物车'
+            }
+        ],
+       // pids='58f97db80304656d7a905d0d,5922b424940282a75fa82ac2'.split(','),
+        pageData={
+        },
+        render=function(){
+            for(var i= 0,n;n=pageConf[i++];){
+                var data=pageData[n.pid]||{};
+                n.data=[];
+                for(var k in data){
+                    n.data.push({
+                        uv:data[k].uv,
+                        pv:data[k].pv,
+                        label:k
+                    })
+                }
+                n.data.sort(function(a,b){return a.label> b.label?-1:1});
+            }
+            $list.html(Mustache.render(tpl,pageConf))
+            //console.log(pageData)
+        },
+        callback=function(data,pid){
+
+            pageData[pid]=data;
+            callback.count=callback.count||0;
+            callback.count++;
+
+            if(callback.count==pids.length){
+
+                render();
+            }
+        };
+
   return {
     init:function($mod){
+        //
+        $list=$('.J_list',$mod);
+        tpl=$('.J_tpl',$mod).html();
+        if($mod.attr('data-env')=='local'){
+            pageData={
+                '58f97db80304656d7a905d0d':{"20170622":{"pv":267,"uv":232},"20170623":{"pv":11,"uv":9}},
+                '5922b424940282a75fa82ac2':{"20170622":{"pv":23,"uv":12},"20170623":{"pv":1,"uv":1}},
+                '58fb40ca0304656d7a905e16':{"20170622":{"pv":3,"uv":2},"20170623":{"pv":0,"uv":0}}
+            }
+        }
+
+        var pids=[];
+        for(var i= 0,n;n=pageConf[i++];){
+            pids.push(n.pid);
+        }
+        for(var i= 0,pid;pid=pids[i++];){
+            getData(pid,callback)
+        }
+        render();
 
     }
   }
--- mod.xsl
+++ mod.xsl
@@ -1,10 +1,50 @@
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:oxm="https://www.openxsl.com">
     <xsl:template match="/root" name="wurui.smct-admin">
         <!-- className 'J_OXMod' required  -->
-        <div class="J_OXMod oxmod-smct-admin" ox-mod="smct-admin">
-            <h1>
-                This is mod smct-admin;
-            </h1>
+        <div class="J_OXMod oxmod-smct-admin" ox-mod="smct-admin" data-env="{env/domain}">
+            <div class="J_list">
+                <table width="100%" cellpadding="0" cellspacing="0">
+                    <tbody>
+                        <tr>
+                            <th colspan="2">首页</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                <span>...</span>
+                                <br/>
+                                <label>今日访客</label>
+                            </td>
+                            <td>
+                                <span>...</span>
+                                <br/>
+                                <label>昨日访客</label>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <script type="text/tpl" class="J_tpl"><![CDATA[
+                <table width="100%" cellpadding="0" cellspacing="0">
+                    <tbody>
+                        {{#.}}
+                        <tr>
+                            <th colspan="2">{{title}}</th>
+                        </tr>
+                        <tr>
+                            {{#data}}
+                            <td>
+                                <span">{{uv}}/{{pv}}</span>
+                                <br/>
+                                <label>{{label}}</label>
+                            </td>
+                            {{/data}}
+                        </tr>
+                        {{/.}}
+                    </tbody>
+                </table>
+                ]]>
+            </script>
+
         </div>
     </xsl:template>
 </xsl:stylesheet>
